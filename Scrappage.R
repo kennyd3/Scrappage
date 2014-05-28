4efa8335470d07245a894e75dbe16eb6db7976f8
Kenneth Davison <kennydavison@gmail.com>
2014-05-28 09:39:14 -0700

    All locations summaries
    updated functions to work for all locations summaries

--- Scrappage.R
+++ Scrappage.R
@@ -88,8 +88,9 @@ gr.or <- c(500001,500003,500004,500007)
 gr.ga <- c(500001,500002,500007)
 
 #function for last month numbers
-rsn.mo <- function(location,grow.area,yr){
+last.mo <- function(yr,location=NULL,grow.area=NULL,n=1){
   gr.rng <- 1:length(grow.area)
+  kbys <- c("Reason","cat")
   mo <- as.numeric(format(Sys.Date(),"%m"))
   if(mo == 1){
     mo <- "12"
@@ -98,138 +99,47 @@ rsn.mo <- function(location,grow.area,yr){
   } else mo <- as.character(mo-1)
   cnt <- 0
   outPut <- list()
-  for(i in gr.rng){
-  temp.a <- a[loc == location & year == yr & month == mo & grw == grow.area[i], 
-              list(EQUs=sum(as.numeric(equ)),Cost=sum(as.numeric(cost))), 
-              keyby = Reason]
-  #add totals row
-  c1 <- "zTotals"
-  c2 <- sum(temp.a$EQUs)
-  c3 <- sum(temp.a$Cost)
-  temp.a$Perc.Tot <- temp.a$Cost/c3
-  c4 <- c2/c2
-  newrow <- data.frame(Reason = c1, EQUs = c2, Cost = c3, Perc.Tot = c4)
-  temp.a <- rbind(temp.a, newrow)
-  cnt <- cnt + 1
-  outPut[[cnt]] <- temp.a
-  }
-  return(outPut)
-}
-#function for YTD cumulative numbers
-rsn.yr <- function(location,grow.area,yr){
-  gr.rng <- 1:length(grow.area)
-  mo <- as.numeric(format(Sys.Date(),"%m"))-1
-  mos <- 1:mo
-  for(i in 1:mo){
-    if(length(mos[i]) == 1){
-      temp.mo <- as.character(paste(0,mos[i],sep=""))
-      } else temp.mo <- as.character(mos[i])
-      mos[i] <- temp.mo
-    }
-  cnt <- 0
-  outPut <- list()
-  for(i in gr.rng){
-    for(n in 1:mo){
-      mnth <- mos[n]
-      temp.a <- a[loc == location & year == yr & month == mnth & grw == grow.area[i], 
+  if(is.null(location)){
+    temp.a <- a[year == yr & month == mo, 
                 list(EQUs=sum(as.numeric(equ)),Cost=sum(as.numeric(cost))), 
-                keyby = Reason]
-        if(n > 1){
-          mrg.1 <- as.data.frame(merge(temp.a,temp.b,by="Reason",all=TRUE))
-          eqs <- ddply(mrg.1,.(Reason), summarise, EQUs=sum(EQUs.x,EQUs.y,na.rm=TRUE))
-          cst <- ddply(mrg.1,.(Reason), summarise, Cost=sum(Cost.x,Cost.y,na.rm=TRUE))
-          temp.a <- as.data.frame(merge(eqs,cst,by="Reason",all=TRUE))
-        }
-      temp.b <- temp.a
-    }
-  #add totals row
-  c1 <- "zTotals"
-  c2 <- sum(temp.a$EQUs)
-  c3 <- sum(temp.a$Cost)
-  temp.a$Perc.Tot <- temp.a$Cost/c3  
-  c4 <- c2/c2
-  newrow <- data.frame(Reason = c1, EQUs = c2, Cost = c3, Perc.Tot = c4)
-  temp.a <- rbind(temp.a, newrow)
-  outPut[[i]] <- temp.a
-  }
-  return(outPut)
-}
-#function for creating a list of growing areas' data
-cr.gr.list <- function(mo.ly,yr.ly,mo.ty,yr.ty){
-  totals <- list()
-  ars.tots <- list()
-  ars <- length(mo.ly)
-  for(n in 1:ars){
-    frames <- list(mo.ly,yr.ly,mo.ty,yr.ty)
-      for(i in 1:4){
-        totals[[i]] <- data.frame(frames[[i]][[n]])
-      }
-    tlln <- length(totals)
-      for(l in 1:tlln){
-        if(l != 1){
-          one <- merge(one, totals[[l]], by="Reason", all=TRUE)
-        } else one <- totals[[1]]
-      setnames(one,"EQUs",paste("EQUs.",l,sep=""))
-      setnames(one,"Cost",paste("Cost.",l,sep=""))
-      setnames(one,"Perc.Tot",paste("Percent.Tot.",l,sep=""))   
-    }
-    ars.tots[[n]] <- one
-  }
-  return(ars.tots)
-}
-
-#month totals last year and this year
-or.mo.ty <- rsn.mo(160000,gr.or,yr)
-or.mo.ly <- rsn.mo(160000,gr.or,yr-1)
-or.yr.ty <- rsn.yr(160000,gr.or,yr)
-or.yr.ly <- rsn.yr(160000,gr.or,yr-1)
-
-ca.mo.ty <- rsn.mo(170000,gr.or,yr)
-ca.mo.ly <- rsn.mo(170000,gr.or,yr-1)
-ca.yr.ty <- rsn.yr(170000,gr.or,yr)
-ca.yr.ly <- rsn.yr(170000,gr.or,yr-1)
-
-ga.mo.ty <- rsn.mo(550000,gr.ga,yr)
-ga.mo.ly <- rsn.mo(550000,gr.ga,yr-1)
-ga.yr.ty <- rsn.yr(550000,gr.ga,yr)
-ga.yr.ly <- rsn.yr(550000,gr.ga,yr-1)
-
-or.rsn <- cr.gr.list(or.mo.ly,or.yr.ly,or.mo.ty,or.yr.ty)
-ca.rsn <- cr.gr.list(ca.mo.ly,ca.yr.ly,ca.mo.ty,ca.yr.ty)
-ga.rsn <- cr.gr.list(ga.mo.ly,ga.yr.ly,ga.mo.ty,ga.yr.ty)
-
-######################################
-#CATEGORY DATA
-######################################
-
-#function for last month numbers
-cat.mo <- function(location,grow.area,yr){
-  gr.rng <- 1:length(grow.area)
-  mo <- as.numeric(format(Sys.Date(),"%m"))-1
-  mo <- paste("0",mo,sep="")
-  cnt <- 0
-  outPut <- list()
-  for(i in gr.rng){
-    temp.a <- a[loc == location & year == yr & month == mo & grw == grow.area[i], 
-                list(EQUs=sum(as.numeric(equ)),Cost=sum(as.numeric(cost))), 
-                keyby = cat]
+                keyby = eval(kbys[n],all=TRUE)]
     #add totals row
     c1 <- "zTotals"
     c2 <- sum(temp.a$EQUs)
     c3 <- sum(temp.a$Cost)
-    temp.a$Perc.Tot <- temp.a$Cost/c3  
+    temp.a$Perc.Tot <- temp.a$Cost/c3
     c4 <- c2/c2
-    newrow <- data.frame(cat = c1, EQUs = c2, Cost = c3, Perc.Tot = c4)
+    if(n==1){
+      newrow <- data.frame(Reason = c1, EQUs = c2, Cost = c3, Perc.Tot = c4)
+    } else newrow <- data.frame(cat = c1, EQUs = c2, Cost = c3, Perc.Tot = c4)
     temp.a <- rbind(temp.a, newrow)
     cnt <- cnt + 1
     outPut[[cnt]] <- temp.a
+  } else {
+    for(i in gr.rng){
+      temp.a <- a[loc == location & year == yr & month == mo & grw == grow.area[i], 
+                  list(EQUs=sum(as.numeric(equ)),Cost=sum(as.numeric(cost))), 
+                  keyby = eval(kbys[n],all=TRUE)]
+      #add totals row
+      c1 <- "zTotals"
+      c2 <- sum(temp.a$EQUs)
+      c3 <- sum(temp.a$Cost)
+      temp.a$Perc.Tot <- temp.a$Cost/c3
+      c4 <- c2/c2
+      if(n==1){
+        newrow <- data.frame(Reason = c1, EQUs = c2, Cost = c3, Perc.Tot = c4)
+      } else newrow <- data.frame(cat = c1, EQUs = c2, Cost = c3, Perc.Tot = c4)
+      temp.a <- rbind(temp.a, newrow)
+      cnt <- cnt + 1
+      outPut[[cnt]] <- temp.a
+    }
   }
   return(outPut)
 }
-
 #function for YTD cumulative numbers
-cat.yr <- function(location,grow.area,yr){
+year.data <- function(yr,location=NULL,grow.area=NULL,b=1){
   gr.rng <- 1:length(grow.area)
+  kbys <- c("Reason","cat")
   mo <- as.numeric(format(Sys.Date(),"%m"))-1
   mos <- 1:mo
   for(i in 1:mo){
@@ -240,17 +150,44 @@ cat.yr <- function(location,grow.area,yr){
   }
   cnt <- 0
   outPut <- list()
-  for(i in gr.rng){
+  if(!is.null(location)){
+    for(i in gr.rng){
+      for(n in 1:mo){
+        mnth <- mos[n]
+        temp.a <- a[loc == location & year == yr & month == mnth & grw == grow.area[i], 
+                    list(EQUs=sum(as.numeric(equ)),Cost=sum(as.numeric(cost))), 
+                    keyby = eval(kbys[b],all=TRUE)]
+        if(n > 1){
+          mrg.1 <- as.data.frame(merge(temp.a,temp.b,by=kbys[b],all=TRUE))
+          eqs <- ddply(mrg.1,kbys[b], summarise, EQUs=sum(EQUs.x,EQUs.y,na.rm=TRUE))
+          cst <- ddply(mrg.1,kbys[b], summarise, Cost=sum(Cost.x,Cost.y,na.rm=TRUE))
+          temp.a <- as.data.frame(merge(eqs,cst,by=kbys[b],all=TRUE))
+        }
+        temp.b <- temp.a
+      }
+      #add totals row
+      c1 <- "zTotals"
+      c2 <- sum(temp.a$EQUs)
+      c3 <- sum(temp.a$Cost)
+      temp.a$Perc.Tot <- temp.a$Cost/c3  
+      c4 <- c2/c2
+      if(b==1){
+        newrow <- data.frame(Reason = c1, EQUs = c2, Cost = c3, Perc.Tot = c4)
+      } else newrow <- data.frame(cat = c1, EQUs = c2, Cost = c3, Perc.Tot = c4)
+      temp.a <- rbind(temp.a, newrow)
+      outPut[[i]] <- temp.a
+    }
+  } else {
     for(n in 1:mo){
       mnth <- mos[n]
-      temp.a <- a[loc == location & year == yr & month == mnth & grw == grow.area[i], 
+      temp.a <- a[year == yr & month == mnth, 
                   list(EQUs=sum(as.numeric(equ)),Cost=sum(as.numeric(cost))), 
-                  keyby = cat]
+                  keyby = eval(kbys[b],all=TRUE)]
       if(n > 1){
-        mrg.1 <- as.data.frame(merge(temp.a,temp.b,by="cat",all=TRUE))
-        eqs <- ddply(mrg.1,.(cat), summarise, EQUs=sum(EQUs.x,EQUs.y,na.rm=TRUE))
-        cst <- ddply(mrg.1,.(cat), summarise, Cost=sum(Cost.x,Cost.y,na.rm=TRUE))
-        temp.a <- as.data.frame(merge(eqs,cst,by="cat",all=TRUE))
+        mrg.1 <- as.data.frame(merge(temp.a,temp.b,by=kbys[b],all=TRUE))
+        eqs <- ddply(mrg.1,kbys[b], summarise, EQUs=sum(EQUs.x,EQUs.y,na.rm=TRUE))
+        cst <- ddply(mrg.1,kbys[b], summarise, Cost=sum(Cost.x,Cost.y,na.rm=TRUE))
+        temp.a <- as.data.frame(merge(eqs,cst,by=kbys[b],all=TRUE))
       }
       temp.b <- temp.a
     }
@@ -260,14 +197,16 @@ cat.yr <- function(location,grow.area,yr){
     c3 <- sum(temp.a$Cost)
     temp.a$Perc.Tot <- temp.a$Cost/c3  
     c4 <- c2/c2
-    newrow <- data.frame(cat = c1, EQUs = c2, Cost = c3, Perc.Tot = c4)
+    if(b==1){
+      newrow <- data.frame(Reason = c1, EQUs = c2, Cost = c3, Perc.Tot = c4)
+    } else newrow <- data.frame(cat = c1, EQUs = c2, Cost = c3, Perc.Tot = c4)
     temp.a <- rbind(temp.a, newrow)
-    outPut[[i]] <- temp.a
+    outPut[[1]] <- temp.a
   }
   return(outPut)
 }
-#function for merging
-cr.gr.list.cat <- function(mo.ly,yr.ly,mo.ty,yr.ty){
+#function for creating a list of growing areas' data
+cr.gr.list <- function(mo.ly,yr.ly,mo.ty,yr.ty,b=1){
   totals <- list()
   ars.tots <- list()
   ars <- length(mo.ly)
@@ -279,7 +218,11 @@ cr.gr.list.cat <- function(mo.ly,yr.ly,mo.ty,yr.ty){
     tlln <- length(totals)
     for(l in 1:tlln){
       if(l != 1){
-        one <- merge(one, totals[[l]], by="cat", all=TRUE)
+        if(b == 1){
+          one <- merge(one, totals[[l]], by="Reason", all=TRUE)
+        } else {
+          one <- merge(one, totals[[l]],by="cat",all=TRUE)
+        }
       } else one <- totals[[1]]
       setnames(one,"EQUs",paste("EQUs.",l,sep=""))
       setnames(one,"Cost",paste("Cost.",l,sep=""))
@@ -291,24 +234,48 @@ cr.gr.list.cat <- function(mo.ly,yr.ly,mo.ty,yr.ty){
 }
 
 #month totals last year and this year
-or.mo.ty.cat <- cat.mo(160000,gr.or,yr)
-or.mo.ly.cat <- cat.mo(160000,gr.or,yr-1)
-or.yr.ty.cat <- cat.yr(160000,gr.or,yr)
-or.yr.ly.cat <- cat.yr(160000,gr.or,yr-1)
+or.mo.ty <- last.mo(yr,160000,gr.or)
+or.mo.ly <- last.mo(yr-1,160000,gr.or)
+or.yr.ty <- year.data(yr,160000,gr.or)
+or.yr.ly <- year.data(yr-1,160000,gr.or)
 
-ca.mo.ty.cat <- cat.mo(170000,gr.or,yr)
-ca.mo.ly.cat <- cat.mo(170000,gr.or,yr-1)
-ca.yr.ty.cat <- cat.yr(170000,gr.or,yr)
-ca.yr.ly.cat <- cat.yr(170000,gr.or,yr-1)
+ca.mo.ty <- last.mo(yr,170000,gr.or)
+ca.mo.ly <- last.mo(yr-1,170000,gr.or)
+ca.yr.ty <- year.data(yr,170000,gr.or)
+ca.yr.ly <- year.data(yr-1,170000,gr.or)
 
-ga.mo.ty.cat <- cat.mo(550000,gr.ga,yr)
-ga.mo.ly.cat <- cat.mo(550000,gr.ga,yr-1)
-ga.yr.ty.cat <- cat.yr(550000,gr.ga,yr)
-ga.yr.ly.cat <- cat.yr(550000,gr.ga,yr-1)
+ga.mo.ty <- last.mo(yr,550000,gr.ga)
+ga.mo.ly <- last.mo(yr-1,550000,gr.ga)
+ga.yr.ty <- year.data(yr,550000,gr.ga)
+ga.yr.ly <- year.data(yr-1,550000,gr.ga)
 
-or.cat <- cr.gr.list.cat(or.mo.ly.cat,or.yr.ly.cat,or.mo.ty.cat,or.yr.ty.cat)
-ca.cat <- cr.gr.list.cat(ca.mo.ly.cat,ca.yr.ly.cat,ca.mo.ty.cat,ca.yr.ty.cat)
-ga.cat <- cr.gr.list.cat(ga.mo.ly.cat,ga.yr.ly.cat,ga.mo.ty.cat,ga.yr.ty.cat)
+or.rsn <- cr.gr.list(or.mo.ly,or.yr.ly,or.mo.ty,or.yr.ty)
+ca.rsn <- cr.gr.list(ca.mo.ly,ca.yr.ly,ca.mo.ty,ca.yr.ty)
+ga.rsn <- cr.gr.list(ga.mo.ly,ga.yr.ly,ga.mo.ty,ga.yr.ty)
+
+######################################
+#CATEGORY DATA
+######################################
+
+#month totals last year and this year
+or.mo.ty.cat <- last.mo(yr,160000,gr.or,n=2)
+or.mo.ly.cat <- last.mo(yr-1,160000,gr.or,n=2)
+or.yr.ty.cat <- year.data(yr,160000,gr.or,b=2)
+or.yr.ly.cat <- year.data(yr-1,160000,gr.or,b=2)
+
+ca.mo.ty.cat <- last.mo(yr,170000,gr.or,n=2)
+ca.mo.ly.cat <- last.mo(yr-1,170000,gr.or,n=2)
+ca.yr.ty.cat <- year.data(yr,170000,gr.or,b=2)
+ca.yr.ly.cat <- year.data(yr-1,170000,gr.or,b=2)
+
+ga.mo.ty.cat <- last.mo(yr,550000,gr.ga,n=2)
+ga.mo.ly.cat <- last.mo(yr-1,550000,gr.ga,n=2)
+ga.yr.ty.cat <- year.data(yr,550000,gr.ga,b=2)
+ga.yr.ly.cat <- year.data(yr-1,550000,gr.ga,b=2)
+
+or.cat <- cr.gr.list(or.mo.ly.cat,or.yr.ly.cat,or.mo.ty.cat,or.yr.ty.cat,b=2)
+ca.cat <- cr.gr.list(ca.mo.ly.cat,ca.yr.ly.cat,ca.mo.ty.cat,ca.yr.ty.cat,b=2)
+ga.cat <- cr.gr.list(ga.mo.ly.cat,ga.yr.ly.cat,ga.mo.ty.cat,ga.yr.ty.cat,b=2)
 
 ########################################
 #Top Ten Lists
@@ -325,29 +292,46 @@ top.ten <- data.table(loc=scrap.2$Loc.,
                       it.desc=scrap.2$Description)
 
 
-top.lists <- function(location,year,month,gr.area){
+top.lists <- function(year,month,location=NULL,gr.area=NULL){
   top.list <- list()
-  for(i in 1:length(gr.area)){
-    top.ten$tot.scrap <- top.ten[loc == location & year == year & month == month & grw == gr.area[i], 
-                                 list(EQUs=sum(as.numeric(equ)))]
-
-    tp.ten <- top.ten[loc == location & year == year & month == month & grw == gr.area[i], 
-                      list(Description=it.desc,
-                          Reason=reason,
-                          Qty=qty,
-                          EQUs=equ,
-                          Cost=cost,
-                          per.tot=equ/tot.scrap), 
-                      keyby = item]
-
-    top.list[[i]] <- tp.ten[order(-EQUs),][1:10]
+  if(!is.null(location)){
+    for(i in 1:length(gr.area)){
+      top.ten$tot.scrap <- top.ten[loc == location & year == year & month == month & grw == gr.area[i], 
+                                   list(EQUs=sum(as.numeric(equ)))]
+      
+      tp.ten <- top.ten[loc == location & year == year & month == month & grw == gr.area[i], 
+                        list(Description=it.desc,
+                             Reason=reason,
+                             Qty=qty,
+                             EQUs=equ,
+                             Cost=cost,
+                             per.tot=equ/tot.scrap), 
+                        keyby = item]
+      
+      top.list[[i]] <- tp.ten[order(-EQUs),][1:10]
+    }
   }
-  return(top.list)
+  if(is.null(location)){
+    top.ten$tot.scrap <- top.ten[year == year & month == month, 
+                                 list(EQUs=sum(as.numeric(equ)))]
+    
+    tp.ten <- top.ten[year == year & month == month, 
+                      list(Description=it.desc,
+                           Reason=reason,
+                           Qty=qty,
+                           EQUs=equ,
+                           Cost=cost,
+                           per.tot=equ/tot.scrap), 
+                      keyby = item]
+    
+    top.list[[1]] <- tp.ten[order(-EQUs),][1:10]
+  }
+  return(top.list)  
 }
 
-or.top <- top.lists(160000,yr,mo,gr.or)
-ca.top <- top.lists(170000,yr,mo,gr.or)
-ga.top <- top.lists(550000,yr,mo,gr.ga)
+or.top <- top.lists(yr,mo,160000,gr.or)
+ca.top <- top.lists(yr,mo,170000,gr.or)
+ga.top <- top.lists(yr,mo,550000,gr.ga)
 
 ######################################
 #Write csvs to file
@@ -360,6 +344,7 @@ while(i < length(files) + 1){
   files[[i]][is.na(files[[i]])]<-0
   i = i + 1 
 }
+
 file.names <- c("or.rsn"=1:4,
                 "ca.rsn"=5:8,
                 "ga.rsn"=9:11,
@@ -379,7 +364,6 @@ while(i < length(files) + 1){
   write.csv(files[[i]],file=file.path)
   i <- i + 1
 }
-
 
 #make summaries
 sum.top.ten <- function(location,year,month){
@@ -402,7 +386,7 @@ or.tp.sum <- sum.top.ten(160000,year,mo)
 ca.tp.sum <- sum.top.ten(170000,year,mo)
 ga.tp.sum <- sum.top.ten(550000,year,mo)
 
-summ <- function(location,df){
+summ <- function(df,location=NULL){
   month <- as.numeric(format(Sys.Date(),"%m"))
   yr <- as.numeric(format(Sys.Date(),"%y"))
   if(month == 1){
@@ -421,8 +405,13 @@ summ <- function(location,df){
   #for month dfs 
   for(i in 1:2){
     years <- as.character(yrs[i])
-    by.mo[[i]] <- df[loc == location & year == years & month == mo,list(EQUs=sum(equ),Cost=sum(cost)),keyby = Reason]
-    by.mo[[i]]$perc <- by.mo[[i]]$EQUs/sum(by.mo[[i]]$EQUs)
+    if(is.null(location)){
+      by.mo[[i]] <- df[year == years & month == mo,list(EQUs=sum(equ),Cost=sum(cost)),keyby = Reason]
+      by.mo[[i]]$perc <- by.mo[[i]]$EQUs/sum(by.mo[[i]]$EQUs)
+    } else {
+      by.mo[[i]] <- df[loc == location & year == years & month == mo,list(EQUs=sum(equ),Cost=sum(cost)),keyby = Reason]
+      by.mo[[i]]$perc <- by.mo[[i]]$EQUs/sum(by.mo[[i]]$EQUs)
+    }
   }
   #for YTD dfs
   mo.rng <- 1:month
@@ -437,8 +426,13 @@ summ <- function(location,df){
   by.yr <- list()
   for(i in 1:2){
     years <- as.character(yrs[i])
-    by.yr[[i]] <- df[loc == location & year == years & month %in% mo.chars ,list(EQUs=sum(equ),Cost=sum(cost)),keyby = Reason]
-    by.yr[[i]]$perc <- by.yr[[i]]$Cost/sum(by.yr[[i]]$Cost)
+    if(is.null(location)){
+      by.yr[[i]] <- df[year == years & month %in% mo.chars ,list(EQUs=sum(equ),Cost=sum(cost)),keyby = Reason]
+      by.yr[[i]]$perc <- by.yr[[i]]$Cost/sum(by.yr[[i]]$Cost)
+    } else {
+      by.yr[[i]] <- df[loc == location & year == years & month %in% mo.chars ,list(EQUs=sum(equ),Cost=sum(cost)),keyby = Reason]
+      by.yr[[i]]$perc <- by.yr[[i]]$Cost/sum(by.yr[[i]]$Cost)
+    }
   }
   mrg.1 <- as.data.frame(merge(by.mo[[1]],by.yr[[1]],by="Reason",all=TRUE))  
   mrg.2 <- as.data.frame(merge(by.mo[[2]],by.yr[[2]],by="Reason",all=TRUE))
@@ -464,9 +458,9 @@ summ <- function(location,df){
   return(mrgr)
 }
 
-or.sum.rsn <- summ(160000,a)
-ca.sum.rsn <- summ(170000,a)
-ga.sum.rsn <- summ(550000,a)
+or.sum.rsn <- summ(a,160000)
+ca.sum.rsn <- summ(a,170000)
+ga.sum.rsn <- summ(a,550000)
 
 files <- list()
 files <- c(or.tp.sum,or.sum.rsn,ca.tp.sum,ca.sum.rsn,ga.tp.sum,ga.sum.rsn) 
